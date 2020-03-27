8f5832a8216255a3accf5cbe02b82da9798fc519
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-03-27 13:15:51 -0400

    Got the form component to catch errors and return the order from the database

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,21 +1,88 @@
-import React from "react";
+import React, {useState} from "react";
+import * as yup from 'yup';
+import axios from 'axios';
+
+const formSchema = yup.object().shape({
+    name: yup.string().required("Please include your name so we know if we have the right person!"),
+    pizzaSize: yup.string().required("Pizza size is required"),
+    // pizzaTopping: yup.string().required("plz"),
+    pepperoni: yup.string(),
+    cheese: yup.string(),
+    sausage: yup.string(),
+    pineapple: yup.string(),
+    instructions: yup.string()
+})
 
 function Order() {
+
+    const [formState, setFormState] = useState({
+        name: '',
+        pizzaSize: '',
+        // pizzaTopping: '',
+        instructions: ''
+    })
+
+    const [errors, setErrors] = useState({
+        name: '',
+        pizzaSize: '',
+        pizzaTopping: '',
+        instructions: ''
+    })
+
+    const [post, setPost] = useState([]);
+
+    const validation = event => {
+        yup.reach(formSchema, event.target.name)
+        .validate(event.target.value)
+        .then(() => {
+            setErrors({
+                ...errors, [event.target.name]: ""
+            })
+        })
+        .catch(err => {
+            setErrors({
+                ...errors, [event.target.name]: err.errors[0]
+            })
+        })
+    
+    }
+
+    const inputChange = event => {
+        event.persist();
+        const newFormData = {
+            ...formState, [event.target.name]: event.target.type === "checkbox" ? event.target.checked : event.target.value
+        }
+        validation(event);
+        setFormState(newFormData);
+    }
+
+    const submit = event => {
+        event.preventDefault();
+        console.log("sending Data")
+        axios.post('https://reqres.in/api/users', formState)
+        .then(response => {
+            setPost(response.data);
+            console.log("success!", formState);
+        })
+    }
     return (
-        <div>
-            <form>
+            <form onSubmit={submit}>
                 <label htmlFor = "name">
                     Name:
                     <input 
                         id = "name"
                         type = "text"
                         name = "name"
+                        value= {formState.name}
+                        onChange= {inputChange}
                     />
+                    {errors.name.length < 0 ? (<p className="error">{errors.name}</p>) : null}
                 </label><br />
 
                 <label htmlFor = "pizzaSize">
                     Select pizza size:
-                    <select id="pizzaSize" name="pizzaSize">
+                    <select id="pizzaSize" name="pizzaSize" value={formState.pizzaSize} onChange={inputChange}>
+                        <option>None</option>
                         <option>Small</option>
                         <option>Medium</option>
                         <option>Large</option>
@@ -24,20 +91,32 @@ function Order() {
 
                 <label htmlFor="pizzaTopping"> Select desired toppings:<br />
                     <input 
+                        id = "pepperoni"
                         name="pepperoni"
                         type="checkbox"
+                        value= {formState.pizzaTopping}
+                        onChange= {inputChange}
                     /> Pepperoni <br />
                     <input 
+                        id = "cheese"
                         name="cheese"
                         type="checkbox"
+                        value= {formState.pizzaTopping}
+                        onChange= {inputChange}
                     /> Cheese <br />
                     <input 
+                        id = "sausage"
                         name="sausage"
                         type="checkbox"
+                        value= {formState.pizzaTopping}
+                        onChange= {inputChange}
                     /> Sausage <br />
                     <input 
+                        id = "pineapple"
                         name="pineapple"
                         type="checkbox"
+                        value= {formState.pizzaTopping}
+                        onChange= {inputChange}
                     /> Pineapple(gross) <br />
 
                 </label><br />
@@ -45,16 +124,17 @@ function Order() {
                 <label htmlFor="instructions">
                     Any special requests?:<br />
                     <input 
-                        name="intructions"
+                        name="instructions"
                         type="text"
+                        value= {formState.instructions}
+                        onChange= {inputChange}
                     />
                 </label><br />
 
-                <label>
+                <pre>{JSON.stringify(post, null, 2)}</pre>
                     <button>Submit Order!</button>
-                </label>
+                
             </form>
-        </div>
     )
 }
 
